807ecd116d38c1d47b000258c4973a5b62376b70
Cecile Muller <cecilemuller@users.noreply.github.com>
2019-12-23 00:08:46 +0100

    chore: prettify sources

--- src/Plugin.js
+++ src/Plugin.js
@@ -1,53 +1,49 @@
-'use strict';
-const {basename, extname, join} = require('path');
-const fg = require('fast-glob');
-const globParent = require('glob-parent');
-const PLUGIN_ID = 'wildpeaks-glob-plugin';
-
+"use strict";
+const {basename, extname, join} = require("path");
+const fg = require("fast-glob");
+const globParent = require("glob-parent");
+const PLUGIN_ID = "wildpeaks-glob-plugin";
 
 class Plugin {
-
-	constructor(options){
-		if (typeof options !== 'object'){
-			throw new Error('The options should be an Object');
+	constructor(options) {
+		if (typeof options !== "object") {
+			throw new Error("The options should be an Object");
 		}
 		let {entries, entriesMap, polyfills} = options;
 
-		if (typeof entries === 'undefined'){
-			entries = './src/*.js';
+		if (typeof entries === "undefined") {
+			entries = "./src/*.js";
 		}
-		if (typeof entries !== 'string'){
+		if (typeof entries !== "string") {
 			throw new Error('Property "entries" should be a string');
 		}
 		this.entries = entries;
 
-		if (typeof entriesMap === 'undefined'){
+		if (typeof entriesMap === "undefined") {
 			entriesMap = filepath => basename(filepath, extname(filepath));
 		}
-		if (typeof entriesMap !== 'function'){
+		if (typeof entriesMap !== "function") {
 			throw new Error('Property "entriesMap" should be a function');
 		}
 		this.entriesMap = entriesMap;
 
-		if (typeof polyfills === 'undefined'){
+		if (typeof polyfills === "undefined") {
 			polyfills = [];
 		}
-		if (!Array.isArray(polyfills)){
+		if (!Array.isArray(polyfills)) {
 			throw new Error('Property "polyfills" should be an array');
 		}
 		this.polyfills = polyfills;
 	}
 
-	apply(compiler){
+	apply(compiler) {
 		compiler.options.entry = () => {
 			const {entries, entriesMap, polyfills} = this;
-			const hasPolyfills = (polyfills.length > 0);
+			const hasPolyfills = polyfills.length > 0;
 			const generated = {};
-			fg
-			.sync([entries], {dot: false, followSymbolicLinks: true, cwd: compiler.context})
-			.forEach(filepath => {
+			fg.sync([entries], {dot: false, followSymbolicLinks: true, cwd: compiler.context}).forEach(filepath => {
 				const name = entriesMap(filepath);
-				if ((typeof name === 'string') && (name !== '')){
+				if (typeof name === "string" && name !== "") {
 					generated[name] = hasPolyfills ? polyfills.concat([filepath]) : filepath;
 				}
 			});
@@ -55,13 +51,9 @@ class Plugin {
 		};
 
 		compiler.hooks.afterCompile.tap(PLUGIN_ID, compilation => {
-			compilation.contextDependencies.add(
-				join(compiler.context, globParent(this.entries))
-			);
+			compilation.contextDependencies.add(join(compiler.context, globParent(this.entries)));
 		});
 	}
-
 }
-
 
 module.exports = Plugin;
